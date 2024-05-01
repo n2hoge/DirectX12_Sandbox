9b593377d8ba784be6b3044b23de0b67426bf92b
nao <xxxxx@gmail.com>
2024-05-01 21:05:05 +0900

    Add project files.

---
+++ DirectX12_Sandbox/Source/main.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+// @brief コンソール画面にフォーマット付き文字列を表示
+// @param format フォーマット　(%d とか %f とか)
+// @param 可変長引数
+// @remarks この関数はデバッグ用です。デバッグ時にしか動作しません
+void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	vprintf(format, valist);
+	va_end(valist);
+#endif
+}
+
+#ifdef _DEBUG
+int main()
+{
+#else
+int WINAPI WinMain(HINSTAJCE, HINSTANCE, LPSTR, int)
+{
+#endif
+	DebugOutputFormatString("Show window test.");
+	getchar();
+	return 0;
+}
+
